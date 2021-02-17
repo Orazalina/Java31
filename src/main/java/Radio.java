fb6c7541bf5b45035b45b787b1acf585a7b595a7
Anara Orazalina <orazalina.anara@inbox.ru>
2021-02-18 01:02:08 +0600

    added

---
+++ src/main/java/Radio.java
@@ -0,0 +1,11 @@
+public class Radio {
+
+    int currentNumberRadioStation = 5;
+    int minNumberRadioStation = 0;
+    int maxNumberRadioStation = 9;
+    int currentVolume = 5;
+    int volumeMin = 0;
+    int volumeMax = 10;
+
+
+}
